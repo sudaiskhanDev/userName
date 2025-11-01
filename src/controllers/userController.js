051f724fc9305ac4aa5f56421c1128e72688b793
sudaiskhanDev <sudaisliveofficial@gmail.com>
2025-11-01 13:46:26 +0500

    loader

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,23 @@
-import User from "@/models/Profile";
+import Profile from "@/models/User";
 
 export async function saveUserController(name) {
   if (!name) return { success: false, message: "Name is required" };
 
-  const user = new User({ name });
+  const user = new Profile({ name });
   await user.save();
 
   return { success: true, message: "User saved", user };
 }
 
 export async function getAllUsersController() {
-  const users = await User.find({});
+  const users = await Profile.find({});
   return users;
 }
 
 export async function deleteUserController(id) {
   if (!id) return { success: false, message: "User ID is required" };
 
-  const deleted = await User.findByIdAndDelete(id);
+  const deleted = await Profile.findByIdAndDelete(id);
   if (!deleted) return { success: false, message: "User not found" };
 
   return { success: true, message: "User deleted" };
